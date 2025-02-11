8eaf867bdf227cdeb58fc7b0ed47a162e1a584b6
mario camayo <mariocamayo17@gmail.com>
2025-02-11 18:03:45 -0500

    se modificó la medida del tamaño del botón de crear una cuenta en el componente Navbar

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -49,7 +49,7 @@ const [token, setToken] = useState(true)
             :
             <button
             onClick={()=>navigate('/login')}
-            className='bg-primary text-white px-2 py-1 rounded-full font-light md:block text-[12px]'>Crea una cuenta</button>
+            className='bg-primary text-white px-4 py-3 rounded-full font-light md:block  sm:px-6 sm:py-4 sm:text-lg sm:w-48'>Sign Up</button>
         }
 
         <img 
